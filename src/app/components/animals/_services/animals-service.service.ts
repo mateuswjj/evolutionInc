9b600cddc377f0367f4d3e07c4780ea6bc915da1
mateus weinert <mateuswhj@gmail.com>
2023-09-30 16:21:37 -0300

    animal service get

--- src/app/components/animals/_services/animals-service.service.ts
+++ src/app/components/animals/_services/animals-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalsServiceService {
+  API_KEY = ''
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAnimalsByName(name: string){
+    const url = 'https://api.api-ninjas.com/v1/animals?name='+ name
+    const headers = new HttpHeaders({ 'X-Api-Key': this.API_KEY });
+
+    0
+    return this.http.get(url, { headers: headers })
+  }
 }
--- src/app/components/animals/animals.component.ts
+++ src/app/components/animals/animals.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnimalsServiceService } from './_services/animals-service.service';
 
 @Component({
   selector: 'app-animals',
   templateUrl: './animals.component.html',
   styleUrls: ['./animals.component.scss']
 })
-export class AnimalsComponent {
+export class AnimalsComponent implements OnInit{
+
+  constructor(public animalService: AnimalsServiceService){
+    
+  }
+
+  ngOnInit() {
+   this.animalService.getAnimalsByName("Lion").subscribe((res) => console.log(res))
+  }
+
+  searchAnimal(animal:any){
+    console.log(animal)
+  }
 //TODO create enviroments
 }
